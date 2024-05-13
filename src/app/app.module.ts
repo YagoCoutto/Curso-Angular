39dd39904cd5fb22a269123f14552a0de0d53280
Yago <62577739+YagoCoutto@users.noreply.github.com>
2024-05-13 17:41:23 -0300

    Curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsModule } from './cards/cards.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/cards/cards.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardButtonComponent } from '../card-button/card-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent
+  ]
+})
+export class CardsModule { }
